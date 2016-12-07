0c9d5c99beff708a8bcde5d6ff908dd9486e4b85
kyekang <cockroach419@gmail.com>
2016-12-07 21:02:40 +0900

    form 틀만 만듬

--- client/admin/components/main/AddFunc.js
+++ client/admin/components/main/AddFunc.js
@@ -3,27 +3,181 @@ import React from 'react'
 
 export default function AddFunc(props) {
   return (
-    <div class="container d-table">
-      <div class="d-100vh-va-middle">
-        <div class="row">
-          <div class="col-md-6 offset-md-3">
-            <div class="clearfix">
-              <h1 class="float-xs-left display-3 mr-2">AddFunc</h1>
-              <h4 class="pt-1">Oops! You're lost. AddFunc</h4>
-              <p class="text-muted">The page you are looking for was not found.</p>
-            </div>
-            <div class="input-prepend input-group">
-              <span class="input-group-addon"><i class="fa fa-search"></i>
-              </span>
-              <input id="prependedInput" class="form-control" size="16" type="text" placeholder="What are you looking for?" />
-              <span class="input-group-btn">
-                  <button class="btn btn-info" type="button">Search</button>
-              </span>
-            </div>
+    <div className="row">
+      <div className="col-md-8">
+        <div className="card">
+          <div className="card-header">
+            <strong>Add</strong> Function
+          </div>
+          <div className="card-block">
+            <form action="" method="post" enctype="multipart/form-data" className="form-horizontal ">
+              <div className="form-group row">
+                <label className="col-md-3 form-control-label" for="text-input">User Name</label>
+                <div className="col-md-9">
+                  <input type="text" id="nuser" name="nuser" className="form-control" placeholder="로그인 생기면 없어집니당"></input>
+                </div>
+              </div>
+              <div className="form-group row">
+                <label className="col-md-3 form-control-label" for="select">Platform</label>
+                <div className="col-md-9">
+                  <select id="select" name="select" className="form-control" size="1">
+                    <option value="node">nodejs</option>
+                  </select>
+                </div>
+              </div>
+              <div className="form-group row">
+                <label className="col-md-3 form-control-label" for="select">Version</label>
+                <div className="col-md-9">
+                  <select id="select" name="select" className="form-control" size="1">
+                    <option value="0">Please select</option>
+                    <option value="6.9">6.9 LTS</option>
+                    <option value="7.2">7.2</option>
+                  </select>
+                </div>
+              </div>
+              <div className="form-group row">
+                <label className="col-md-3 form-control-label" for="text-input">Function Name</label>
+                <div className="col-md-9">
+                  <input type="text" id="nfunc" name="nfunc" className="form-control" placeholder="함수 이름"></input>
+                </div>
+              </div>
+              <div className="form-group row">
+                <label className="col-md-3 form-control-label" for="textarea-input">Function</label>
+                <div className="col-md-9">
+                  <textarea id="textarea-input" name="textarea-input" rows="9" className="form-control" placeholder="Function..."></textarea>
+                </div>
+              </div>
+            </form>
+          </div>
+          <div className="card-footer">
+            <button type="submit" className="btn btn-sm btn-primary"><i className="fa fa-dot-circle-o"></i> Submit</button>
           </div>
         </div>
       </div>
     </div>
+              /*
+              <div className="form-group row">
+                <label className="col-md-3 form-control-label" for="text-input">Text Input</label>
+                <div className="col-md-9">
+                  <input type="text" id="text-input" name="text-input" className="form-control" placeholder="Text">
+                    <span className="help-block">This is a help text</span>
+                  </input>
+                </div>
+              </div>
+              <div className="form-group row">
+                <label className="col-md-3 form-control-label" for="email-input">Email Input</label>
+                <div className="col-md-9">
+                  <input type="email" id="email-input" name="email-input" className="form-control" placeholder="Enter Email">
+                    <span className="help-block">Please enter your email</span>
+                  </input>
+                </div>
+              </div>
+              <div className="form-group row">
+                <label className="col-md-3 form-control-label" for="password-input">Password</label>
+                <div className="col-md-9">
+                  <input type="password" id="password-input" name="password-input" className="form-control" placeholder="Password" >
+                    <span className="help-block">Please enter a complex password</span>
+                  </input>
+                </div>
+              </div>
+              <div className="form-group row">
+                <label className="col-md-3 form-control-label" for="textarea-input">Textarea</label>
+                <div className="col-md-9">
+                  <textarea id="textarea-input" name="textarea-input" rows="9" className="form-control" placeholder="Content.."></textarea>
+                </div>
+              </div>
+              <div className="form-group row">
+                <label className="col-md-3 form-control-label" for="select">Select</label>
+                <div className="col-md-9">
+                  <select id="select" name="select" className="form-control" size="1">
+                    <option value="0">Please select</option>
+                    <option value="1">Option #1</option>
+                    <option value="2">Option #2</option>
+                    <option value="3">Option #3</option>
+                  </select>
+                </div>
+              </div>
+              <div className="form-group row">
+                <label className="col-md-3 form-control-label" for="select">Select Large</label>
+                <div className="col-md-9">
+                  <select id="select" name="select" className="form-control input-lg" size="1">
+                    <option value="0">Please select</option>
+                    <option value="1">Option #1</option>
+                    <option value="2">Option #2</option>
+                    <option value="3">Option #3</option>
+                  </select>
+                </div>
+              </div>
+              <div className="form-group row">
+                <label className="col-md-3 form-control-label" for="select">Select Small</label>
+                <div className="col-md-9">
+                  <select id="select" name="select" className="form-control input-sm" size="1">
+                    <option value="0">Please select</option>
+                    <option value="1">Option #1</option>
+                    <option value="2">Option #2</option>
+                    <option value="3">Option #3</option>
+                  </select>
+                </div>
+              </div>
+              <div className="form-group row">
+                <label className="col-md-3 form-control-label" for="select">Disabled Select</label>
+                <div className="col-md-9">
+                  <select id="disabledSelect" className="form-control" disabled="">
+                    <option value="0">Please select</option>
+                    <option value="1">Option #1</option>
+                    <option value="2">Option #2</option>
+                    <option value="3">Option #3</option>
+                  </select>
+                </div>
+              </div>
+              <div className="form-group row">
+                <label className="col-md-3 form-control-label">Radios</label>
+                <div className="col-md-9">
+                  <div className="radio">
+                    <label for="radio1">
+                      <input type="radio" id="radio1" name="radios" value="option1">Option 1</input>
+                    </label>
+                  </div>
+                  <div className="radio">
+                    <label for="radio2">
+                      <input type="radio" id="radio2" name="radios" value="option2">Option 2</input>
+                    </label>
+                  </div>
+                  <div className="radio">
+                    <label for="radio3">
+                      <input type="radio" id="radio3" name="radios" value="option3">Option 3</input>
+                    </label>
+                  </div>
+                </div>
+              </div>
+              <div className="form-group row">
+                <label className="col-md-3 form-control-label">Inline Radios</label>
+                <div className="col-md-9">
+                  <label className="radio-inline" for="inline-radio1">
+                    <input type="radio" id="inline-radio1" name="inline-radios" value="option1">One</input>
+                  </label>
+                  <label className="radio-inline" for="inline-radio2">
+                    <input type="radio" id="inline-radio2" name="inline-radios" value="option2">Two</input>
+                  </label>
+                  <label className="radio-inline" for="inline-radio3">
+                    <input type="radio" id="inline-radio3" name="inline-radios" value="option3">Three</input>
+                  </label>
+                </div>
+              </div>
+              <div className="form-group row">
+                <label className="col-md-3 form-control-label" for="file-input">File input</label>
+                <div className="col-md-9">
+                  <input type="file" id="file-input" name="file-input" />
+                </div>
+              </div>
+          </div>
+          <div className="card-footer">
+            <button type="submit" className="btn btn-sm btn-primary"><i className="fa fa-dot-circle-o"></i> Submit</button>
+            <button type="reset" className="btn btn-sm btn-danger"><i className="fa fa-ban"></i> Reset</button>
+          </div>
+        </div>
+      </div>
+    </div>*/
   )
 }
 
--- dist/admin.bundle.js
+++ dist/admin.bundle.js
@@ -3600,58 +3600,264 @@
 	function AddFunc(props) {
 	  return _react2.default.createElement(
 	    "div",
-	    { "class": "container d-table" },
+	    { className: "row" },
 	    _react2.default.createElement(
 	      "div",
-	      { "class": "d-100vh-va-middle" },
+	      { className: "col-md-8" },
 	      _react2.default.createElement(
 	        "div",
-	        { "class": "row" },
+	        { className: "card" },
 	        _react2.default.createElement(
 	          "div",
-	          { "class": "col-md-6 offset-md-3" },
+	          { className: "card-header" },
 	          _react2.default.createElement(
-	            "div",
-	            { "class": "clearfix" },
-	            _react2.default.createElement(
-	              "h1",
-	              { "class": "float-xs-left display-3 mr-2" },
-	              "AddFunc"
-	            ),
-	            _react2.default.createElement(
-	              "h4",
-	              { "class": "pt-1" },
-	              "Oops! You're lost. AddFunc"
-	            ),
-	            _react2.default.createElement(
-	              "p",
-	              { "class": "text-muted" },
-	              "The page you are looking for was not found."
-	            )
+	            "strong",
+	            null,
+	            "Add"
 	          ),
+	          " Function"
+	        ),
+	        _react2.default.createElement(
+	          "div",
+	          { className: "card-block" },
 	          _react2.default.createElement(
-	            "div",
-	            { "class": "input-prepend input-group" },
+	            "form",
+	            { action: "", method: "post", enctype: "multipart/form-data", className: "form-horizontal " },
 	            _react2.default.createElement(
-	              "span",
-	              { "class": "input-group-addon" },
-	              _react2.default.createElement("i", { "class": "fa fa-search" })
-	            ),
-	            _react2.default.createElement("input", { id: "prependedInput", "class": "form-control", size: "16", type: "text", placeholder: "What are you looking for?" }),
-	            _react2.default.createElement(
-	              "span",
-	              { "class": "input-group-btn" },
+	              "div",
+	              { className: "form-group row" },
 	              _react2.default.createElement(
-	                "button",
-	                { "class": "btn btn-info", type: "button" },
-	                "Search"
+	                "label",
+	                { className: "col-md-3 form-control-label", "for": "text-input" },
+	                "User Name"
+	              ),
+	              _react2.default.createElement(
+	                "div",
+	                { className: "col-md-9" },
+	                _react2.default.createElement("input", { type: "text", id: "nuser", name: "nuser", className: "form-control", placeholder: "\uB85C\uADF8\uC778 \uC0DD\uAE30\uBA74 \uC5C6\uC5B4\uC9D1\uB2C8\uB2F9" })
+	              )
+	            ),
+	            _react2.default.createElement(
+	              "div",
+	              { className: "form-group row" },
+	              _react2.default.createElement(
+	                "label",
+	                { className: "col-md-3 form-control-label", "for": "select" },
+	                "Platform"
+	              ),
+	              _react2.default.createElement(
+	                "div",
+	                { className: "col-md-9" },
+	                _react2.default.createElement(
+	                  "select",
+	                  { id: "select", name: "select", className: "form-control", size: "1" },
+	                  _react2.default.createElement(
+	                    "option",
+	                    { value: "node" },
+	                    "nodejs"
+	                  )
+	                )
+	              )
+	            ),
+	            _react2.default.createElement(
+	              "div",
+	              { className: "form-group row" },
+	              _react2.default.createElement(
+	                "label",
+	                { className: "col-md-3 form-control-label", "for": "select" },
+	                "Version"
+	              ),
+	              _react2.default.createElement(
+	                "div",
+	                { className: "col-md-9" },
+	                _react2.default.createElement(
+	                  "select",
+	                  { id: "select", name: "select", className: "form-control", size: "1" },
+	                  _react2.default.createElement(
+	                    "option",
+	                    { value: "0" },
+	                    "Please select"
+	                  ),
+	                  _react2.default.createElement(
+	                    "option",
+	                    { value: "6.9" },
+	                    "6.9 LTS"
+	                  ),
+	                  _react2.default.createElement(
+	                    "option",
+	                    { value: "7.2" },
+	                    "7.2"
+	                  )
+	                )
+	              )
+	            ),
+	            _react2.default.createElement(
+	              "div",
+	              { className: "form-group row" },
+	              _react2.default.createElement(
+	                "label",
+	                { className: "col-md-3 form-control-label", "for": "text-input" },
+	                "Function Name"
+	              ),
+	              _react2.default.createElement(
+	                "div",
+	                { className: "col-md-9" },
+	                _react2.default.createElement("input", { type: "text", id: "nfunc", name: "nfunc", className: "form-control", placeholder: "\uD568\uC218 \uC774\uB984" })
+	              )
+	            ),
+	            _react2.default.createElement(
+	              "div",
+	              { className: "form-group row" },
+	              _react2.default.createElement(
+	                "label",
+	                { className: "col-md-3 form-control-label", "for": "textarea-input" },
+	                "Function"
+	              ),
+	              _react2.default.createElement(
+	                "div",
+	                { className: "col-md-9" },
+	                _react2.default.createElement("textarea", { id: "textarea-input", name: "textarea-input", rows: "9", className: "form-control", placeholder: "Function..." })
 	              )
 	            )
+	          )
+	        ),
+	        _react2.default.createElement(
+	          "div",
+	          { className: "card-footer" },
+	          _react2.default.createElement(
+	            "button",
+	            { type: "submit", className: "btn btn-sm btn-primary" },
+	            _react2.default.createElement("i", { className: "fa fa-dot-circle-o" }),
+	            " Submit"
 	          )
 	        )
 	      )
 	    )
-	  );
+	  )
+	  /*
+	  <div className="form-group row">
+	    <label className="col-md-3 form-control-label" for="text-input">Text Input</label>
+	    <div className="col-md-9">
+	      <input type="text" id="text-input" name="text-input" className="form-control" placeholder="Text">
+	        <span className="help-block">This is a help text</span>
+	      </input>
+	    </div>
+	  </div>
+	  <div className="form-group row">
+	    <label className="col-md-3 form-control-label" for="email-input">Email Input</label>
+	    <div className="col-md-9">
+	      <input type="email" id="email-input" name="email-input" className="form-control" placeholder="Enter Email">
+	        <span className="help-block">Please enter your email</span>
+	      </input>
+	    </div>
+	  </div>
+	  <div className="form-group row">
+	    <label className="col-md-3 form-control-label" for="password-input">Password</label>
+	    <div className="col-md-9">
+	      <input type="password" id="password-input" name="password-input" className="form-control" placeholder="Password" >
+	        <span className="help-block">Please enter a complex password</span>
+	      </input>
+	    </div>
+	  </div>
+	  <div className="form-group row">
+	    <label className="col-md-3 form-control-label" for="textarea-input">Textarea</label>
+	    <div className="col-md-9">
+	      <textarea id="textarea-input" name="textarea-input" rows="9" className="form-control" placeholder="Content.."></textarea>
+	    </div>
+	  </div>
+	  <div className="form-group row">
+	    <label className="col-md-3 form-control-label" for="select">Select</label>
+	    <div className="col-md-9">
+	      <select id="select" name="select" className="form-control" size="1">
+	        <option value="0">Please select</option>
+	        <option value="1">Option #1</option>
+	        <option value="2">Option #2</option>
+	        <option value="3">Option #3</option>
+	      </select>
+	    </div>
+	  </div>
+	  <div className="form-group row">
+	    <label className="col-md-3 form-control-label" for="select">Select Large</label>
+	    <div className="col-md-9">
+	      <select id="select" name="select" className="form-control input-lg" size="1">
+	        <option value="0">Please select</option>
+	        <option value="1">Option #1</option>
+	        <option value="2">Option #2</option>
+	        <option value="3">Option #3</option>
+	      </select>
+	    </div>
+	  </div>
+	  <div className="form-group row">
+	    <label className="col-md-3 form-control-label" for="select">Select Small</label>
+	    <div className="col-md-9">
+	      <select id="select" name="select" className="form-control input-sm" size="1">
+	        <option value="0">Please select</option>
+	        <option value="1">Option #1</option>
+	        <option value="2">Option #2</option>
+	        <option value="3">Option #3</option>
+	      </select>
+	    </div>
+	  </div>
+	  <div className="form-group row">
+	    <label className="col-md-3 form-control-label" for="select">Disabled Select</label>
+	    <div className="col-md-9">
+	      <select id="disabledSelect" className="form-control" disabled="">
+	        <option value="0">Please select</option>
+	        <option value="1">Option #1</option>
+	        <option value="2">Option #2</option>
+	        <option value="3">Option #3</option>
+	      </select>
+	    </div>
+	  </div>
+	  <div className="form-group row">
+	    <label className="col-md-3 form-control-label">Radios</label>
+	    <div className="col-md-9">
+	      <div className="radio">
+	        <label for="radio1">
+	          <input type="radio" id="radio1" name="radios" value="option1">Option 1</input>
+	        </label>
+	      </div>
+	      <div className="radio">
+	        <label for="radio2">
+	          <input type="radio" id="radio2" name="radios" value="option2">Option 2</input>
+	        </label>
+	      </div>
+	      <div className="radio">
+	        <label for="radio3">
+	          <input type="radio" id="radio3" name="radios" value="option3">Option 3</input>
+	        </label>
+	      </div>
+	    </div>
+	  </div>
+	  <div className="form-group row">
+	    <label className="col-md-3 form-control-label">Inline Radios</label>
+	    <div className="col-md-9">
+	      <label className="radio-inline" for="inline-radio1">
+	        <input type="radio" id="inline-radio1" name="inline-radios" value="option1">One</input>
+	      </label>
+	      <label className="radio-inline" for="inline-radio2">
+	        <input type="radio" id="inline-radio2" name="inline-radios" value="option2">Two</input>
+	      </label>
+	      <label className="radio-inline" for="inline-radio3">
+	        <input type="radio" id="inline-radio3" name="inline-radios" value="option3">Three</input>
+	      </label>
+	    </div>
+	  </div>
+	  <div className="form-group row">
+	    <label className="col-md-3 form-control-label" for="file-input">File input</label>
+	    <div className="col-md-9">
+	      <input type="file" id="file-input" name="file-input" />
+	    </div>
+	  </div>
+	  </div>
+	  <div className="card-footer">
+	  <button type="submit" className="btn btn-sm btn-primary"><i className="fa fa-dot-circle-o"></i> Submit</button>
+	  <button type="reset" className="btn btn-sm btn-danger"><i className="fa fa-ban"></i> Reset</button>
+	  </div>
+	  </div>
+	  </div>
+	  </div>*/
+	  ;
 	}
 
 /***/ },
